8bd0f2b206c62aca83f75cd3c83edf638262519f
Jacob Baca <jbaca@stitchdata.com>
2020-01-22 20:04:34 +0000

    bumped version and updated changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-kustomer',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the Kustomer v1.0 API',
       author='scott.coleman@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
