8a11588786307a6e415e57369d10e45d67c6947d
Ian Benedict <ianbenedict@gmail.com>
2023-08-23 16:28:09 -0400

    :star: Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-kustomer',
-      version='1.0.5',
+      version='1.0.6',
       description='Singer.io tap for extracting data from the Kustomer v1.0 API',
       author='scott.coleman@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
